a2797b6ffe9fe3545f4d93a690d25c24c075e463
netsamma <netsamma@gmail.com>
2022-04-12 19:32:00 +0200

    Login

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Login() {
+  return (
+	<div>
+		<form>
+			<input type="email" />
+			<input type="password" />
+		</form>
+	</div>
+  )
+}
+
+export default Login
